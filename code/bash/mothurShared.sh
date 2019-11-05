9f4fe75de79cc793cbd7d466d97fc18a5af39fcd
William L. Close <wclose@users.noreply.github.com>
2019-11-05 10:55:52 -0500

    cleaning up old code

--- code/bash/mothurShared.sh
+++ code/bash/mothurShared.sh
@@ -13,12 +13,9 @@ export SAMPLEDIR=${1:?ERROR: Need to define SAMPLEDIR.}
 export SILVAV4=${2:?ERROR: Need to define SILVAV4.}
 export RDPFASTA=${3:?ERROR: Need to define RDPFASTA.}
 export RDPTAX=${4:?ERROR: Need to define RDPTAX.}
-# export MOCKGROUPS=${5:?ERROR: Need to define MOCKGROUPS.} # List of mock groups in raw data dir separated by '-'
-# export CONTROLGROUPS=${6:?ERROR: Need to define CONTROLGROUPS.} # List of control groups in raw data dir separated by '-'
 
 # Other variables
 export OUTDIR=data/mothur/process/
-export COMBINEDGROUPS=$(echo "${MOCKGROUPS}"-"${CONTROLGROUPS}") # Combines the list of mock and control groups into a single string separated by '-'
 
 
 
@@ -61,43 +58,6 @@ mv "${OUTDIR}"/*.opti_mcc.shared "${OUTDIR}"/final.shared
 mv "${OUTDIR}"/*.cons.taxonomy "${OUTDIR}"/final.taxonomy
 mv "${OUTDIR}"/*.0.03.rep.fasta "${OUTDIR}"/final.rep.seqs
 mv "${OUTDIR}"/*.0.03.rep.count_table "${OUTDIR}"/final.rep.count_table
-
-
-
-# ####################################
-# # Make Group-Specific Shared Files #
-# ####################################
-
-# # Sample shared file
-# echo PROGRESS: Creating sample shared file.
-
-# # Removing all mock and control groups from shared file leaving only samples
-# mothur "#remove.groups(shared="${OUTDIR}"/final.shared, groups="${COMBINEDGROUPS}")"
-
-# # Renaming output file
-# mv "${OUTDIR}"/final.0.03.pick.shared "${OUTDIR}"/sample.final.shared
-
-
-
-# # Mock shared file
-# echo PROGRESS: Creating mock shared file.
-
-# # Removing non-mock groups from shared file
-# mothur "#get.groups(shared="${OUTDIR}"/final.shared, groups="${MOCKGROUPS}")"
-
-# # Renaming output file
-# mv "${OUTDIR}"/final.0.03.pick.shared "${OUTDIR}"/mock.final.shared
-
-
-
-# # Control shared file
-# echo PROGRESS: Creating control shared file.
-
-# # Removing any non-control groups from shared file
-# mothur "#get.groups(shared="${OUTDIR}"/final.shared, groups="${CONTROLGROUPS}")"
-
-# # Renaming output file
-# mv "${OUTDIR}"/final.0.03.pick.shared "${OUTDIR}"/control.final.shared
 
 
 
