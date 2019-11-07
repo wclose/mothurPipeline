ff6d395c4f5e8699c6245316418a03790cc3992e
William Close <close.will@gmail.com>
2019-11-06 22:12:12 -0500

    fixing issue with rabund file cleanup by doing analysis in tmp dirs

--- code/bash/mothurAlpha.sh
+++ code/bash/mothurAlpha.sh
@@ -11,10 +11,11 @@
 # Set the variables to be used in this script
 export SHARED=${1:?ERROR: Need to define SHARED.} # Shared file
 export COUNT=${2:?ERROR: Need to define COUNT.} # Count file generated from shared file
-export ALPHA=${3:?ERROR: Need to define ALPHA.} # Names of mothur beta metrics joined by hyphens
+export SUBTHRESH=${3:?ERROR: Need to define SUBTHRESH.} # Setting threshold for minimum number of reads to subsample
+export ALPHA=${4:?ERROR: Need to define ALPHA.} # Names of mothur alpha metrics joined by hyphens
 
-# Setting threshold for minimum number of reads to subsample
-export SUBTHRESH=1000
+# Other settings
+export OUTDIR=$(echo "${SHARED}" | sed 's:\(.*/\).*:\1:')
 
 
 
@@ -31,6 +32,22 @@ READCOUNT=$(awk -v SUBTHRESH="${SUBTHRESH}" '$2 >= SUBTHRESH { print $2}' "${COU
 # Run diversity analysis on new aligned data set
 echo PROGRESS: Calculating alpha diversity and subsampling to "${READCOUNT}" reads.
 
-# Calculating alpha diversity
+# Creating tmp directory for easier clean up and to avoid conflicts with rarefaction rabund files
+TMP="${OUTDIR}"/tmp_$(date +%s)/
+mkdir -p "${TMP}"/
+
+# Copying shared file to tmp directory so output files will be placed there
+cp "${SHARED}" "${TMP}"/
+
+# Finding path to shared file in new location
+TMP_SHARED=$(echo "${SHARED}" | sed 's:'"${OUTDIR}"':'"${TMP}"':')
+
+# Calculating alpha and beta diversity
 # If a sample doesn't have enough reads, it'll be eliminated from the analysis
-mothur "#summary.single(shared="${SHARED}", calc="${ALPHA}", subsample="${READCOUNT}")"
+mothur "#summary.single(shared="${TMP_SHARED}", calc="${ALPHA}", subsample="${READCOUNT}")"
+
+# Moving output to main output dir
+mv "${TMP}"/*groups*summary "${OUTDIR}"
+
+# Cleaning up
+rm -rf "${TMP}"/
--- code/bash/mothurRarefaction.sh
+++ code/bash/mothurRarefaction.sh
@@ -11,6 +11,9 @@
 # Set the variables to be used in this script
 export SHARED=${1:?ERROR: Need to define SHARED}
 
+# Other settings
+export OUTDIR=$(echo "${SHARED}" | sed 's:\(.*/\).*:\1:')
+
 
 
 ###########################
@@ -20,5 +23,21 @@ export SHARED=${1:?ERROR: Need to define SHARED}
 # Generating rarefaction files
 echo PROGRESS: Generating rarefaction tables.
 
+# Creating tmp directory for easier clean up and to avoid conflicts with rarefaction rabund files
+TMP="${OUTDIR}"/tmp_$(date +%s)/
+mkdir -p "${TMP}"/
+
+# Copying shared file to tmp directory so output files will be placed there
+cp "${SHARED}" "${TMP}"/
+
+# Finding path to shared file in new location
+TMP_SHARED=$(echo "${SHARED}" | sed 's:'"${OUTDIR}"':'"${TMP}"':')
+
 # Calculating rarefaction curve data
-mothur "#rarefaction.single(shared="${SHARED}", calc=sobs, freq=100)"
+mothur "#rarefaction.single(shared="${TMP_SHARED}", calc=sobs, freq=100)"
+
+# Moving output to main output dir
+mv "${TMP}"/*groups*rarefaction "${OUTDIR}"
+
+# Cleaning up
+rm -rf "${TMP}"/
