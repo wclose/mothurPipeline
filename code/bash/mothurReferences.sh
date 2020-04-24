b12e621ea5272dc2ebd9a9f027c219575100e5b0
William L. Close <close.will@gmail.com>
2020-04-24 15:25:01 -0400

    updating location/name of mothur files and switching to curl

--- code/bash/mothurReferences.sh
+++ code/bash/mothurReferences.sh
@@ -9,7 +9,7 @@
 ##################
 
 # Other variables
-export OUTDIR=data/mothur/references/ # Directory for storing mothur reference files
+OUTDIR=data/mothur/references/ # Directory for storing mothur reference files
 
 
 
@@ -27,18 +27,18 @@ mkdir -p "${OUTDIR}"/ "${OUTDIR}"/tmp/
 echo PROGRESS: Preparing SILVA database v4 sequence alignment files. 
 
 # Downloading the prepared SILVA database from the mothur website
-# This version is from v132 and described at http://blog.mothur.org/2018/01/10/SILVA-v132-reference-files/
-# v132 contains 8,517 bacterial sequences
-wget -N -P "${OUTDIR}"/tmp/ https://www.mothur.org/w/images/7/71/Silva.seed_v132.tgz
+# For more inforamtion see https://mothur.org/wiki/silva_reference_files/
+curl -L -R -o "${OUTDIR}"/tmp/silva.nr_v132.tgz -z "${OUTDIR}"/tmp/silva.nr_v132.tgz https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.nr_v132.tgz
 
 # Decompressing the database
-tar xvzf "${OUTDIR}"/tmp/Silva.seed_v132.tgz -C "${OUTDIR}"/tmp/
+tar -xvzf "${OUTDIR}"/tmp/silva.nr_v132.tgz -C "${OUTDIR}"/tmp/
 
 # Using mothur to pull out bacterial sequences and remove sequence gaps
-mothur "#get.lineage(fasta="${OUTDIR}"/tmp/silva.seed_v132.align, taxonomy="${OUTDIR}"/tmp/silva.seed_v132.tax, taxon=Bacteria);degap.seqs(fasta="${OUTDIR}"/tmp/silva.seed_v132.pick.align, processors=8)"
+mothur "#get.lineage(fasta="${OUTDIR}"/tmp/silva.nr_v132.align, taxonomy="${OUTDIR}"/tmp/silva.nr_v132.tax, taxon=Bacteria);
+	degap.seqs(fasta="${OUTDIR}"/tmp/silva.nr_v132.pick.align, processors=8)"
 
 # Renaming the output file and moving it from the tmp dir to the output dir
-mv "${OUTDIR}"/tmp/silva.seed_v132.pick.align "${OUTDIR}"/silva.seed.align
+mv "${OUTDIR}"/tmp/silva.nr_v132.pick.align "${OUTDIR}"/silva.seed.align
 
 # Using mothur to only keep sequences from the v4 region of the 16S rRNA DNA region
 mothur "#pcr.seqs(fasta="${OUTDIR}"/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
@@ -51,14 +51,14 @@ mv "${OUTDIR}"/silva.seed.pcr.align "${OUTDIR}"/silva.v4.align
 echo PROGRESS: Preparing Ribosomal Database Project taxonomy files. 
 
 # Downloading the prepared RDP database from the mothur website
-# For more information see http://blog.mothur.org/2017/03/15/RDP-v16-reference_files/
-wget -N -P "${OUTDIR}"/tmp/ https://www.mothur.org/w/images/c/c3/Trainset16_022016.pds.tgz
+# For more information see https://mothur.org/wiki/rdp_reference_files/
+curl -L -R -o "${OUTDIR}"/tmp/trainset16_022016.rdp.tgz -z "${OUTDIR}"/tmp/trainset16_022016.rdp.tgz https://mothur.s3.us-east-2.amazonaws.com/wiki/trainset16_022016.rdp.tgz
 
 # Decompressing the database
-tar xvzf "${OUTDIR}"/tmp/Trainset16_022016.pds.tgz -C "${OUTDIR}"/tmp/
+tar -xvzf "${OUTDIR}"/tmp/trainset16_022016.rdp.tgz -C "${OUTDIR}"/tmp/
 
 # Move the taxonomy files out of the tmp dir
-mv "${OUTDIR}"/tmp/trainset16_022016.pds/trainset16_022016* "${OUTDIR}"/
+mv "${OUTDIR}"/tmp/trainset16_022016.rdp/trainset16_022016* "${OUTDIR}"/
 
 
 
