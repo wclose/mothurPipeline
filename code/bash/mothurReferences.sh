558c3b86bad25ea88c5952094ba7ad04c078570e
William L. Close <close.will@gmail.com>
2020-08-11 00:14:30 -0400

    updating reference set

--- code/bash/mothurReferences.sh
+++ code/bash/mothurReferences.sh
@@ -28,18 +28,18 @@ echo PROGRESS: Preparing SILVA database v4 sequence alignment files.
 
 # Downloading the prepared SILVA database from the mothur website
 # For more inforamtion see https://mothur.org/wiki/silva_reference_files/
-curl -L -R -o "${OUTDIR}"/tmp/silva.nr_v132.tgz -z "${OUTDIR}"/tmp/silva.nr_v132.tgz https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.nr_v132.tgz
+curl -L -R -o "${OUTDIR}"/tmp/silva.seed_v138.tgz -z "${OUTDIR}"/tmp/silva.seed_v138.tgz https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138.tgz
 
 # Decompressing the database
-tar -xvzf "${OUTDIR}"/tmp/silva.nr_v132.tgz -C "${OUTDIR}"/tmp/
+tar -xvzf "${OUTDIR}"/tmp/silva.seed_v138.tgz -C "${OUTDIR}"/tmp/
 
 # Using mothur to pull out bacterial sequences and only keep sequences from the v4 region of the 16S rRNA DNA region
-mothur "#get.lineage(fasta="${OUTDIR}"/tmp/silva.nr_v132.align, taxonomy="${OUTDIR}"/tmp/silva.nr_v132.tax, taxon=Bacteria);
+mothur "#get.lineage(fasta="${OUTDIR}"/tmp/silva.seed_v138.align, taxonomy="${OUTDIR}"/tmp/silva.seed_v138.tax, taxon=Bacteria);
 	pcr.seqs(fasta=current, start=11894, end=25319, keepdots=F, processors=8)"
 
 # Renaming the final output files
-mv "${OUTDIR}"/tmp/silva.nr_v132.pick.align "${OUTDIR}"/silva.nr.align
-mv "${OUTDIR}"/tmp/silva.nr_v132.pick.pcr.align "${OUTDIR}"/silva.v4.align
+mv "${OUTDIR}"/tmp/silva.seed_v138.pick.align "${OUTDIR}"/silva.seed.align
+mv "${OUTDIR}"/tmp/silva.seed_v138.pick.pcr.align "${OUTDIR}"/silva.v4.align
 
 
 
